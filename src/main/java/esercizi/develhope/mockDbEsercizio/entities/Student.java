ff1dc320358ceeb7ba4dd84b2feb015e76c738a1
floralgreen <carducci.federico20@gmail.com>
2024-03-13 11:48:29 +0100

    commit

--- src/main/java/esercizi/develhope/mockDbEsercizio/entities/Student.java
+++ src/main/java/esercizi/develhope/mockDbEsercizio/entities/Student.java
@@ -18,4 +18,45 @@ public class Student {
     @Column(unique = true, nullable = false)
     private String email;
 
+
+    public Student(){}
+
+    public Student(Long id, String lastName, String firstName, String email) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
